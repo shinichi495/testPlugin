dab54621d74da11e6fd459c75a0ddeae1141a095
Nam Pham <namph2501@gmail.com>
2023-11-25 00:43:08 +0700

    new

--- lib/hdb_sdk_pvoil.dart
+++ lib/hdb_sdk_pvoil.dart
@@ -3,26 +3,38 @@ import 'package:flutter/material.dart';
 
 /// A Calculator.
 class CustomButton extends StatelessWidget {
-  var onPressed;
-  final Widget child;
-  var style;
-  CustomButton({required Key key, @required this.onPressed, required this.child, this.style})
-      : super(key: key);
 
+  CustomButton({super.key});
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.all(16.0),
-          primary: Colors.white,
-          backgroundColor: Colors.blue,
-          elevation: 9.0,
-          textStyle: const TextStyle(
-            fontSize: 20,
-          ),
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
         ),
-        child: child
+      ),
     );
   }
 }
